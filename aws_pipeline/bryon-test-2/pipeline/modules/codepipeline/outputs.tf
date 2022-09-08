e8c7dc332aa4a53e9655b07d757b1a11e9d2d5db
Bryon Smith <webmastersmith37@gmail.com>
2022-09-08 16:13:36 -0500

    minor change

--- aws_pipeline/bryon-test-2/pipeline/modules/codepipeline/outputs.tf
+++ aws_pipeline/bryon-test-2/pipeline/modules/codepipeline/outputs.tf
@@ -1,8 +1,12 @@
 output "codepipeline_configs" {
-    value = {
-        codepipeline = aws_codepipeline.codepipeline.arn
-    }
+  value = {
+    codepipeline = aws_codepipeline.codepipeline.arn
+  }
 }
 output "deployment_role_arn" {
-    value = aws_iam_role.lambda_codebuild_role.arn
+  value = aws_iam_role.lambda_codebuild_role.arn
+}
+
+output "s3_name" {
+  value = aws_s3_bucket.codepipeline_bucket.bucket_domain_name
 }
--- aws_pipeline/bryon-test-2/pipeline/outputs.tf
+++ aws_pipeline/bryon-test-2/pipeline/outputs.tf
@@ -1,9 +1,12 @@
 output "codepipeline" {
-    value = module.codepipeline.codepipeline_configs.codepipeline
+  value = module.codepipeline.codepipeline_configs.codepipeline
 }
 output "codecommit" {
-    value = module.codecommit.codecommit_configs.clone_repository_url
+  value = module.codecommit.codecommit_configs.clone_repository_url
 }
 output "ecrrepo" {
-    value = module.ecr.ecr_configs.ecr_repo_url
+  value = module.ecr.ecr_configs.ecr_repo_url
+}
+output "s3" {
+  value = module.codepipeline.s3_name
 }
