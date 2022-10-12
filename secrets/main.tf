54ed003e194e1728a82ac3b1ccdb2ef08e65a92f
Bryon Smith <webmastersmith37@gmail.com>
2022-10-11 19:10:30 -0500

    minor change

--- secrets/main.tf
+++ secrets/main.tf
@@ -3,27 +3,34 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.29.0"
+      version = "~> 4.34.0"
       #Will allow installation of 4.29.1 and 4.29.10 but not 4.30.0
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.4.2"
+      version = "3.4.3"
     }
   }
 }
 
-resource "random_string" "suffix" {
-  length  = 4
-  special = false
-  upper   = false
-  numeric = false
-  lower   = true
+provider "aws" {
+  region = "us-east-2"
+}
+
+# resource "random_string" "suffix" {
+#   length  = 4
+#   special = false
+#   upper   = false
+#   numeric = false
+#   lower   = true
+# }
+resource "random_pet" "name" {
+  length = 1 # number of words
 }
 
 # create secret name
-resource "aws_secretsmanager_secret" "sonarcloud" {
-  name = "example_secret-${random_string.suffix.result}"
+resource "aws_secretsmanager_secret" "pet" {
+  name = "my_secret-${random_pet.name.id}"
   tags = {
     "managed" : "Terraform"
   }
@@ -31,22 +38,23 @@ resource "aws_secretsmanager_secret" "sonarcloud" {
 
 # create secret value
 resource "aws_secretsmanager_secret_version" "value" {
-  secret_id     = aws_secretsmanager_secret.sonarcloud.id
+  secret_id     = aws_secretsmanager_secret.pet.id
   secret_string = "example-string-to-protect!$%^"
 }
 
 
 # outputs
 output "secret_arn" {
-  value = aws_secretsmanager_secret.sonarcloud.id
+  value = aws_secretsmanager_secret.pet.id
 }
 
-# # secret must be created first.
-# data "aws_secretsmanager_secret_version" "secret-version" {
-#   secret_id = aws_secretsmanager_secret.sonarcloud.id
-# }
-# # this will not print secret, to view secret: terraform output -json
-# output "secret_string" {
-#   value     = data.aws_secretsmanager_secret_version.secret-version.secret_string
-#   sensitive = true
-# }
+# secret must be created first.
+data "aws_secretsmanager_secret_version" "secret-version" {
+  secret_id = aws_secretsmanager_secret.pet.id
+}
+
+# this will not print secret, to view secret: terraform output secret_string
+output "secret_string" {
+  value     = data.aws_secretsmanager_secret_version.secret-version.secret_string
+  sensitive = true
+}
